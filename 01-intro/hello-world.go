8b3028385755bae186a5793513cf83b062698053
Magesh Kuppan <tkmagesh77@gmail.com>
2021-06-21 13:02:32 +0530

    variable declarations

--- 01-intro/hello-world.go
+++ 01-intro/hello-world.go
@@ -1,16 +1,66 @@
 /* package declaration */
 package main
 
-/* import packages */
 import "fmt"
 
+/* import packages */
+
 /* package level variables */
+var name, msg = "Magesh", "Have a nice day!"
 
 /* package init function */
 
 /* main function */
 func main() {
-	fmt.Println("Hello World")
+	/*
+		var msg string
+		msg = "Hello World"
+	*/
+
+	/*
+		var msg string = "Hello World"
+	*/
+
+	/*
+		var msg = "Hello World"
+	*/
+	//the following syntax is applicable only in a function (not in a package level variable)
+	/*
+		msg := "Hello World"
+	*/
+	//fmt.Println(msg)
+
+	//declaraing multiple variables
+	/*
+		var name string
+		var msg string
+	*/
+	/*
+		var (
+			name string
+			msg  string
+		)
+	*/
+	/*
+		var (
+			name, msg string
+		)
+	*/
+	/*
+		var name, msg string
+		name = "Magesh"
+		msg = "Have a nice day!"
+	*/
+	/*
+		var name, msg string = "Magesh", "Have a nice day!"
+	*/
+	/*
+		var name, msg = "Magesh", "Have a nice day!"
+	*/
+	/*
+		name, msg := "Magesh", "Have a nice day!"
+	*/
+	fmt.Println(name, msg)
 }
 
 /* other functions */
