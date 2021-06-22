6cbc75315a4efa3c9aaaad991fef4c1bf3ada502
Magesh Kuppan <tkmagesh77@gmail.com>
2021-06-22 17:33:34 +0530

    Day-02 complete

--- 07-structs/program.go
+++ 07-structs/program.go
@@ -30,7 +30,8 @@ func main() {
 	//var p Product = Product{Id: 102, Name: "Pen", Cost: 5, Units: 20, Category: "Stationary"}
 	var p Product = Product{102, "Pen", 5, 20, "Stationary"}
 	fmt.Println(p)
-	applyDiscount(&p, 10)
+	//applyDiscount(p, 10)
+	p.applyDiscount(10)
 	fmt.Println(p) //=> p.Cost should reflect the discount
 
 	//composition
@@ -44,13 +45,32 @@ func main() {
 	//Part-2
 	//grapes := PerishableProduct{Product: Product{102, "Grapes", 70, 10, "Food"}, Expiry: "2 Days"}
 	grapes := PerishableProduct{Product{102, "Grapes", 70, 10, "Food"}, "2 Days"}
+	//grapes := NewPerishableProduct(102, "Grapes", 70, 10, "Food", "2 Days")
 	fmt.Println(grapes)
 	//fmt.Println("Cost of grapes => ", grapes.Product.Cost)
 	fmt.Println("Cost of grapes => ", grapes.Cost)
 
+	//applying discount using the 'applyDiscount' function
+	//applyDiscount(&(grapes.Product), 20)
+
+	//applying discount using the 'applyDiscount' method
+	grapes.applyDiscount(20)
+	fmt.Println("After applying 20% discount, grapes => ", grapes)
+
 }
 
+//function
 func applyDiscount(p *Product, discount float32) {
 	//(*p).Cost = (*p).Cost * ((100 - discount) / 100)
 	p.Cost = p.Cost * ((100 - discount) / 100)
+}
+
+//method
+func (p *Product) applyDiscount(discount float32) {
+	//(*p).Cost = (*p).Cost * ((100 - discount) / 100)
+	p.Cost = p.Cost * ((100 - discount) / 100)
+}
+
+func NewPerishableProduct(id int, name string, cost float32, units int, category string, expiry string) *PerishableProduct {
+	return &PerishableProduct{Product{id, name, cost, units, category}, expiry}
 }
