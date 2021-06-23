eeab2f2a6f644cbf05f8d016fb57e0a245cfa00c
Magesh Kuppan <tkmagesh77@gmail.com>
2021-06-23 15:44:13 +0530

    assignment - solution

--- 10-interfaces/program-3.go
+++ 10-interfaces/program-3.go
@@ -1,17 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
-func main(){
-	fmt.Println(sum(10,20)) 			//=> 30
-	fmt.Println(sum(10,20,30,40))		//=> 100
-	fmt.Println(sum(10,20,"30",40))		//=> 100
-	fmt.Println(sum(10,20,"30",40,"abc")) //=>100
-	fmt.Println(sum(10,20,int[]{30,40})) //=> 100
-	fmt.Println(sum(10,20,[]interface{30,40, []int{50, 60}})) //=> 210
-	fmt.Println(sum(10,20,[]interface{30,40, []interface{"50", 60, "abc"}})) //=> 210
+func main() {
+	fmt.Println(sum(10, 20))                                                        //=> 30
+	fmt.Println(sum(10, 20, 30, 40))                                                //=> 100
+	fmt.Println(sum(10, 20, "30", 40))                                              //=> 100
+	fmt.Println(sum(10, 20, "30", 40, "abc"))                                       //=>100
+	fmt.Println(sum(10, 20, []int{30, 40}))                                         //=> 100
+	fmt.Println(sum(10, 20, []interface{}{30, 40, []int{50, 60}}))                  //=> 210
+	fmt.Println(sum(10, 20, []interface{}{30, 40, []interface{}{"50", 60, "abc"}})) //=> 210
 }
 
-func sum(/*  */) int {
-
+func sum(nos ...interface{}) int {
+	result := 0
+	for _, val := range nos {
+		switch val.(type) {
+		case int:
+			result += val.(int)
+		case string:
+			if x, err := strconv.Atoi(val.(string)); err == nil {
+				result += x
+			}
+		case []int:
+			values := val.([]int)
+			intValList := make([]interface{}, len(values))
+			for idx, x := range values {
+				intValList[idx] = x
+			}
+			result += sum(intValList...)
+		case []interface{}:
+			result += sum(val.([]interface{})...)
+		}
+	}
+	return result
 }
