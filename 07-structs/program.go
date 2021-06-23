fb940a066bc70c1ab44352999bb45ada67627421
Magesh Kuppan <tkmagesh77@gmail.com>
2021-06-23 09:48:44 +0530

    structs & structs composition

--- 07-structs/program.go
+++ 07-structs/program.go
@@ -10,16 +10,15 @@ type Product struct {
 	Category string
 }
 
-//composition
-//part-1
+//struct composition
+//part - 1
 /*
 type PerishableProduct struct {
-	product Product
+	Prod Product
 	Expiry  string
 }
 */
 
-//part-2
 type PerishableProduct struct {
 	Product
 	Expiry string
@@ -27,50 +26,25 @@ type PerishableProduct struct {
 
 func main() {
 	//var p Product
-	//var p Product = Product{Id: 102, Name: "Pen", Cost: 5, Units: 20, Category: "Stationary"}
-	var p Product = Product{102, "Pen", 5, 20, "Stationary"}
+	//var p Product = Product{}
+	//p := Product{}
+	//p := Product{Id: 102, Name: "Pen", Cost: 5, Units: 50, Category: "Stationary"}
+	p := Product{102, "Pen", 5, 50, "Stationary"}
 	fmt.Println(p)
-	//applyDiscount(p, 10)
-	p.applyDiscount(10)
-	fmt.Println(p) //=> p.Cost should reflect the discount
+	applyDiscount(&p, 10)
+	fmt.Println("After applying 10% discount, product => ", p)
 
-	//composition
-	//part-1
-	/* grapes := PerishableProduct{product: Product{102, "Grapes", 70, 10, "Food"}, Expiry: "2 Days"} */
-	/*
-		grapes := PerishableProduct{Product{102, "Grapes", 70, 10, "Food"}, "2 Days"}
-		fmt.Println(grapes)
-		fmt.Println("Cost of grapes => ", grapes.product.Cost)
-	*/
-	//Part-2
-	//grapes := PerishableProduct{Product: Product{102, "Grapes", 70, 10, "Food"}, Expiry: "2 Days"}
-	grapes := PerishableProduct{Product{102, "Grapes", 70, 10, "Food"}, "2 Days"}
-	//grapes := NewPerishableProduct(102, "Grapes", 70, 10, "Food", "2 Days")
-	fmt.Println(grapes)
-	//fmt.Println("Cost of grapes => ", grapes.Product.Cost)
-	fmt.Println("Cost of grapes => ", grapes.Cost)
+	//struct composition
+	//grapes := PerishableProduct{Prod : Product{105, "Grapes", 50, 20, "Food"}, Expiry: "2 Days"}
+	grapes := PerishableProduct{Product{105, "Grapes", 50, 20, "Food"}, "2 Days"}
+	fmt.Println("Perishable Product (grapes) => ", grapes)
 
-	//applying discount using the 'applyDiscount' function
-	//applyDiscount(&(grapes.Product), 20)
-
-	//applying discount using the 'applyDiscount' method
-	grapes.applyDiscount(20)
-	fmt.Println("After applying 20% discount, grapes => ", grapes)
-
+	//applying discount for perishable product
+	applyDiscount(&(grapes.Product), 10)
+	fmt.Println("After applying 10% discount for Perishable product (grapes) => ", grapes)
 }
 
-//function
-func applyDiscount(p *Product, discount float32) {
-	//(*p).Cost = (*p).Cost * ((100 - discount) / 100)
-	p.Cost = p.Cost * ((100 - discount) / 100)
-}
-
-//method
-func (p *Product) applyDiscount(discount float32) {
-	//(*p).Cost = (*p).Cost * ((100 - discount) / 100)
-	p.Cost = p.Cost * ((100 - discount) / 100)
-}
-
-func NewPerishableProduct(id int, name string, cost float32, units int, category string, expiry string) *PerishableProduct {
-	return &PerishableProduct{Product{id, name, cost, units, category}, expiry}
+//utility function
+func applyDiscount(product *Product, discount float32) {
+	product.Cost = product.Cost * ((100 - discount) / 100)
 }
